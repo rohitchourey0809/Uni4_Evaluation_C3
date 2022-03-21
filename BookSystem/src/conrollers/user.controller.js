1ae9e6bacd3a838aff713993e5a2ee39d31bf203
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-03-21 11:38:39 +0530

    Update user.controller.js

--- BookSystem/src/conrollers/user.controller.js
+++ BookSystem/src/conrollers/user.controller.js
@@ -2,37 +2,45 @@ const User = require('../models/user.model')
 const express = require('express')
 const router = express.Router()
 
-router.post("/",async function (req, res){
-   try{
-    let userdata = await User.create(req.body)
-    console.log(userdata)
-    return res.send(userdata)
-   }
-   catch(err){
-    res.send(err)
-   }
-})
+
 
 router.post("/",
 body("firstName")
 .trim()
 .not()
 .isEmpty()
-.islength({min:3},{max:30}))
+.islength({min:3},{max:30}),
 
-router.post("/",
+
 body("lastName")
 .trim()
 .not()
 .isEmpty()
-.islength({min:3},{max:30}))
+.islength({min:3},{max:30}),
 
-router.post("/",
+
 body("age")
-.trim()
 .not()
 .isEmpty()
-.islength({min:1},{max:150}))
+.withMessage("Age cannot be empty")
+.isNumeric()
+.withMessage("Age must be a number between 1 and 150")
+.custom((val) => {
+  if (val < 1 || val > 150) {
+    throw new Error("Incorrect age provided");
+  }
+  return true;
+}),
+async function (req, res){
+    try{
+     let userdata = await User.create(req.body)
+     console.log(userdata)
+     return res.send(userdata)
+    }
+    catch(err){
+     res.send(err)
+    }
+ });
 
 
 module.exports = router ;
